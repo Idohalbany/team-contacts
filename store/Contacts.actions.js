228a8f68d794f7b5a7ef9aada94857c76fb1cbc1
Nati <7natifel@gmail.com>
2023-09-19 20:30:42 +0300

    F/ContactDetails/Added remove functionality

--- store/Contacts.actions.js
+++ store/Contacts.actions.js
@@ -1,1 +1,13 @@
+import { contactsService } from "../services/contacts.service.local.js"
+import { CONTACT_UNDO, REMOVE_CONTACT } from "./Contacts.reducer.js"
+import { store } from "./store.js"
 
+export function removeContact(contactId) {
+    store.dispatch({ type: REMOVE_CONTACT, contactId })
+    return contactsService.remove(contactId)
+        .catch(err => {
+            store.dispatch({ type: CONTACT_UNDO })
+            console.error('Cannot remove contact:', err)
+            throw err
+        })
+}
