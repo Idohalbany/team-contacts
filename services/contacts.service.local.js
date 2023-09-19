d81f7b9b7a540cd8500c3d67a61a87567da8384a
Ido Halbany <idohalbany97@gmail.com>
2023-09-19 17:31:57 +0300

    updated data model

--- services/contacts.service.local.js
+++ services/contacts.service.local.js
@@ -58,13 +58,13 @@ function remove(contactId) {
 // default todo //
 
 function getEmptyContacts() {
-  return { name: '', mail: '', phone: '' }
+  return { fitstName: '', lastName: '', mail: '', phone: '', desc: '' }
 }
 
 // default filter //
 
 function getDefaultFilter() {
-  return { name: '', mail: '', phone: '' }
+  return { txt: '', mail: '', phone: '' }
 }
 
 // create todos //
@@ -73,12 +73,37 @@ function _createContacts() {
   let contacts = utilService.loadFromStorage(STORAGE_KEY)
   if (!contacts || !contacts.length) {
     contacts = [
-      { name: 'Ido Halbany', mail: 'ido@mail.com', phone: '0548849292', id: utilService.makeId() },
       {
-        name: 'Nati Feldbaum',
+        fitstName: 'Ido',
+        lastName: 'Halbany',
+        mail: 'ido@mail.com',
+        phone: '0548849292',
+        desc: 'Fullstack dev',
+        _id: utilService.makeId(),
+      },
+      {
+        fitstName: 'Nati',
+        lastName: 'Feldbaum',
         mail: 'nati@mail.com',
         phone: '0544839492',
-        id: utilService.makeId(),
+        desc: 'Fullstack dev',
+        _id: utilService.makeId(),
+      },
+      {
+        fitstName: 'Alfi',
+        lastName: 'Al',
+        mail: 'alfi@mail.com',
+        phone: '0544849292',
+        desc: 'Fullstack dev',
+        _id: utilService.makeId(),
+      },
+      {
+        fitstName: 'Gal',
+        lastName: 'Fel',
+        mail: 'gal@mail.com',
+        phone: '0544499492',
+        desc: 'Fullstack dev',
+        _id: utilService.makeId(),
       },
     ]
     utilService.saveToStorage(STORAGE_KEY, contacts)
